e32d45a8f89e1d0b18f48c952224e6df4e33acc1
Martin Vu <mnv_93@hotmail.com>
2025-02-12 13:19:17 +0100

    Add simple search filter

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,16 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
 import ContactItem from "./ContactItem";
+import { ContactInfo } from "../types";
 
 function ContactList() {
+  const [searchItem, setSearchItem] = useState("");
   const { contacts } = useContext(AppContext);
+  const [filteredContacts, setFilteredContacts] = useState<ContactInfo[]>([]);
 
+  useEffect(() => {
+    setFilteredContacts(contacts);
+  }, [contacts]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+    setSearchItem(searchTerm);
+    const filteredItems = contacts.filter(
+      (c) =>
+        c.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        c.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredContacts(filteredItems);
+  };
+  
   return (
     <div>
       <h1>Contacts</h1>
+      <input
+        type="text"
+        value={searchItem}
+        onChange={handleSearch}
+        placeholder="Type to search"
+      ></input>
       <ul className="contact-list">
-        {contacts.map((contact, index) => (        
-          <ContactItem contact={contact} key={index}/>
+        {filteredContacts.map((contact, index) => (
+          <ContactItem contact={contact} key={index} />
         ))}
       </ul>
     </div>
